5a07ef27e60535e07d9bf1efde090c0dfe7bbd5f
jackshinshin <curryisalegend@gmail.com>
2022-04-05 00:11:13 +0800

    Add the functionality of shopping cart

--- cart/urls.py
+++ cart/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from .views import CartView
+from . import views
+
+app_name = 'cart'
 urlpatterns = [
-    path('', CartView.as_view(), name = 'cart')
+    path('', CartView.as_view(), name = 'Cart'),
+    path('add_cart/<int:product_id>/', views.add_cart, name = 'cart_addition')
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,7 +1,39 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
 
+from django.urls import reverse
+from django.shortcuts import redirect
+from django.views.generic import TemplateView, ListView
+
+from .models import CartItem, Cart
+from store.models import Product
 # Create your views here.
+def _cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
+def add_cart(request, product_id):
+    product = Product.objects.get(id = product_id)
+    try:
+        cart = Cart.objects.get(cart_id = _cart_id(request))
+    except:
+        cart = Cart.objects.create(
+            cart_id = _cart_id(request)
+        )
+    cart.save()
 
-class CartView(TemplateView):
+    try:
+        cart_item = CartItem.objects.get(product = product, cart = cart)
+        cart_item.quantity += 1
+        cart_item.save()
+    except:
+        cart_item = CartItem.objects.create(
+            product = product,
+            quantity = 1,
+            cart = cart
+        )
+    return redirect(reverse('cart:Cart'))
+# class AddCart()
+class CartView(ListView):
+    model = CartItem
     template_name = 'store/cart.html'
+    context_object_name = 'current_items'
